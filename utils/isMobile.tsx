7850b7d8ee9b35f9dabd76d41a9f2ba2451f0774
Oussama Abderraouf Attia <92870205+OussamaAbderraoufAttia@users.noreply.github.com>
2024-07-17 23:37:23 +0100

    isMobile is added

---
+++ utils/isMobile.tsx
@@ -0,0 +1,8 @@
+// utils/isMobile.ts
+export const isMobile = () => {
+    if (typeof window === 'undefined') {
+      return false;
+    }
+    return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  };
+  
